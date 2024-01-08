5cfaa9f22c396e48ddde151eed882ebfa6f57268
mati1r <mati1r@wp.pl>
2024-01-08 13:02:21 +0100

    Migrations and models

---
+++ app/Models/Order.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Order extends Model
+{
+    use HasFactory;
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function address()
+    {
+        return $this->belongsTo(Address::class);
+    }
+
+    public function payment()
+    {
+        return $this->hasOne(Payment::class);
+    }
+
+    public function books()
+    {
+        return $this->belongsToMany(Book::class)->withPivot('amount');
+    }
+}
